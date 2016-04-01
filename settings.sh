74ea575f3028d4b29e07ae341a80e51087632692
enkore <public@enkore.de>
2016-04-01 21:43:16 +0200

    Reduce data set

--- settings.sh
+++ settings.sh
@@ -2,9 +2,10 @@ ARCHIVES=(
     #"tar xf",linux-2.6.32.71,https://cdn.kernel.org/pub/linux/kernel/v2.6/longterm/v2.6.32/linux-2.6.32.71.tar.xz
     #"tar xf",linux-3.4.111,https://cdn.kernel.org/pub/linux/kernel/v3.x/linux-3.4.111.tar.xz
     #"tar xf",linux-4.1.20,https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.1.20.tar.xz
-    linux-4.4.6,https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.4.6.tar.xz
+
+    #linux-4.4.6,https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.4.6.tar.xz
     linux-4.5,https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.5.tar.xz
-    silesia,https://pstore.enkore.de/silesia.tar.xz
+    #silesia,https://pstore.enkore.de/silesia.tar.xz
 )
 
 DOWNLOAD=$PWD/downloaded
